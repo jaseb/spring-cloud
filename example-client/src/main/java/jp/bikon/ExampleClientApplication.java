6503f1cff9cbbb4445d305a2ce26445bdfc881a7
jase <jase@bikon.jp>
2017-01-07 12:40:12 +1100

    Setup ExampleClientApplication to use Eureka discovery and cloud configuration

--- example-client/src/main/java/jp/bikon/ExampleClientApplication.java
+++ example-client/src/main/java/jp/bikon/ExampleClientApplication.java
@@ -2,8 +2,10 @@ package jp.bikon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ExampleClientApplication {
 
 	public static void main(String[] args) {
