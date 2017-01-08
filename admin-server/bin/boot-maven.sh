d238d55d74ba0d0c25b2ce51024753afd4664a5d
jase <jase@bikon.jp>
2017-01-08 11:25:34 +1100

    Amend scripts

--- admin-server/bin/boot-maven.sh
+++ admin-server/bin/boot-maven.sh
@@ -10,7 +10,7 @@ curl https://start.spring.io/starter.zip \
   -d type=maven-project \
   -d applicationName="AdminServerApplication" \
   -d packageName="jp.bikon" \
-  -d groupId="jp.bikon" -d name="admin" -d artifactId="admin" \
+  -d groupId="jp.bikon" -d name="Admin Server" -d artifactId="admin-server" \
   -d description="Spring Boot Admin Server" \
   -d dependencies=cloud-eureka \
   -d version="0.2.0-SNAPSHOT" \
